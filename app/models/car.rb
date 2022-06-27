108acec54e08387564e3e3acf078f49d314a20d7
vishwas <vishwas1224@gmail.com>
2022-06-27 21:37:03 +0530

    defined model level scopes for filter

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,8 @@
 class Car < ApplicationRecord
+  ## Associations
   belongs_to :brand
+
+  ## Scopes
+  scope :filter_by_model, ->(name: ''){ where("brands.name = ? ", name) unless name.blank? }
+  scope :filter_by_price, ->(min, max){ where("cars.price BETWEEN ? AND ? ", min, max) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  RESULT_PER_PAGE = 10 
+  ## Associations
   has_many :user_preferred_brands, dependent: :destroy
   has_many :preferred_brands, through: :user_preferred_brands, source: :brand
 end
